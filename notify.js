65cef1d9cf5b844356ef8f5cbb101f532d35717b
createch <github@createchwebdesign.com>
2012-08-28 19:24:04 -0400

    Readded notify.js to remove keys
    Note that notify.js is now completely hidden from the history

---
+++ notify.js
@@ -0,0 +1,31 @@
+console.log("hai from notify.js");
+
+var express			= require('express'),
+	TwilioClient	= require('twilio').Client,
+	Twiml			= require('twilio').Twiml,
+	keys			= require('./keys'),
+	client			= new TwilioClient(
+						keys.twilio.sid,
+						keys.twilio.auth_token,
+						keys.twilio.host),
+	phone			= client.getPhoneNumber('+18134341117'),
+	logger			= require('./log');
+
+exports.send = function(usfClass) {
+
+	var message = messages.success.call(usfClass);
+	
+	phone.setup(function() {
+		phone.sendSms(keys.twilio.myphone, message, {}, function(res) {
+			logger.write(res);
+		});
+	});
+
+};
+
+var messages = {
+	success: function() {
+		var msg = this.seats + " seats in " + this.crn + " at " + this.days + " " + this.time + ". " + this.courseNum + " is " + this.status;
+		return msg;
+	}
+};
